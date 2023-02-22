46c82d33c72cfd719a18ef9f7a1bb8c243e8031f
Lek Peng <lekpeng96@hotmail.com>
2023-02-23 00:34:12 +0800

    fix: remove "any" type from promise

--- src/utils/read.tsx
+++ src/utils/read.tsx
@@ -1,18 +1,22 @@
 import Papa from "papaparse";
+import { csvBooking } from "../Types";
 
-const csvFileToJson = async (filePath: string): Promise<any[]> => {
+const csvFileToJson = async (filePath: string): Promise<csvBooking[]> => {
   const response = await fetch(filePath);
   const content = await response.text();
   const results = await Papa.parse(content, {
     header: true,
     skipEmptyLines: true,
   });
-  return results.data;
+
+  const csvBookings = results.data as csvBooking[];
+  return csvBookings;
 };
 
-async function csvFilesToJson(filePaths: string[]): Promise<any[]> {
+async function csvFilesToJson(filePaths: string[]): Promise<csvBooking[]> {
   const promises = filePaths.map(csvFileToJson);
   const results = await Promise.all(promises);
+
   return results.flat();
 }
 
